f68e9d88f72af07ac8b6d7283c244860f918b7cf
Nikola Masic <137811255+NikolaMasic@users.noreply.github.com>
2025-03-03 22:49:22 +0100

    script.js

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ var color1 = document.querySelector('.color1')
 var color2 = document.querySelector('.color2')
 var body = document.getElementById('gradient')
 
-console.log(gradient);
-
-
 function linearGradient(){
     body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
 }
-
 color1.addEventListener('input', linearGradient);
    
-
 color2.addEventListener('input', linearGradient);
